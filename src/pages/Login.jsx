36d191ac8bf5ca846eaf83f6d5ff9e71016b1e39
Douglas Marcal <douglasymarcal@gmail.com>
2022-02-03 18:33:44 -0300

    Adiciona logica para fazer verificacao do nome de usuario

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      isInvalidName: true,
+    };
+
+    this.verifyName = this.verifyName.bind(this);
+  }
+
+  verifyName({ target: { value } }) {
+    this.setState(() => ({
+      name: value,
+    }), () => this.setState((prevState) => {
+      const MIN_CHARACTERS = 3;
+      if (prevState.name.length >= MIN_CHARACTERS) {
+        return { isInvalidName: false };
+      }
+      return { isInvalidName: true };
+    }));
+  }
+
   render() {
+    const { isInvalidName } = this.state;
     return (
       <div data-testid="page-login">
-        <input data-testid="login-submit-input" type="text" />
-        <button data-testid="login-submit-button" type="button">Entrar</button>
+        <input
+          data-testid="login-name-input"
+          type="text"
+          onChange={ this.verifyName }
+        />
+        <button
+          data-testid="login-submit-button"
+          type="button"
+          disabled={ isInvalidName }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
