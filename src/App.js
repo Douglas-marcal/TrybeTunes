3cbf9ae4175d33ca3b2a18222567ad1bfa909c1e
Douglas Marcal <douglasymarcal@gmail.com>
2022-02-03 17:20:38 -0300

    Adiciona rota para o componente `Album`

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Login, Search } from './pages';
+import { Album, Login, Search } from './pages';
 
 class App extends React.Component {
   render() {
@@ -10,6 +10,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={ Login } />
           <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
         </Switch>
       </BrowserRouter>
     );
