581e8099fba667b24dc6213ed07d43f0c11ad2ce
Douglas Marcal <douglasymarcal@gmail.com>
2022-02-03 18:04:40 -0300

    Adiciona um botão e um campo para fazer o login no componente `Login`

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class Login extends Component {
   render() {
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <input data-testid="login-submit-input" type="text" />
+        <button data-testid="login-submit-button" type="button">Entrar</button>
+      </div>
     );
   }
 }
