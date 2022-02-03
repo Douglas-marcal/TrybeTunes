5d9830f603514e5a0462179edd16819afcc91957
Douglas Marcal <douglasymarcal@gmail.com>
2022-02-03 17:41:07 -0300

    Cria componente `ProfileEdit`

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Album from './Album';
 import Favorites from './Favorites';
 import Login from './Login';
 import Profile from './Profile';
+import ProfileEdit from './ProfileEdit';
 import Search from './Search';
 
-export { Album, Favorites, Login, Profile, Search };
+export { Album, Favorites, Login, Profile, ProfileEdit, Search };
