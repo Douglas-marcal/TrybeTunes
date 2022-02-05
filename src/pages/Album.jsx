77e8e2ec56c98121e34b6afe4668fe35d4ff8042
Douglas Marcal <douglasymarcal@gmail.com>
2022-02-05 11:29:33 -0300

    Renderiza na tela nome do artista e o nome do album

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -7,6 +7,10 @@ class Album extends Component {
   constructor() {
     super();
 
+    this.state = {
+      musics: [],
+    };
+
     this.requestMusic = this.requestMusic.bind(this);
   }
 
@@ -16,13 +20,25 @@ class Album extends Component {
 
   requestMusic() {
     const { match: { params: { id } } } = this.props;
-    getMusics(id).then((data) => data);
+    getMusics(id).then((musics) => {
+      this.setState(() => ({ musics }));
+    });
   }
 
   render() {
+    const { musics } = this.state;
+    const [firstArtist] = musics;
+    let artistName;
+    let collectionName;
+    if (firstArtist) {
+      artistName = firstArtist.artistName;
+      collectionName = firstArtist.collectionName;
+    }
     return (
       <div data-testid="page-album">
         <Header />
+        <h2 data-testid="artist-name">{artistName}</h2>
+        <h3 data-testid="album-name">{collectionName}</h3>
       </div>
     );
   }
