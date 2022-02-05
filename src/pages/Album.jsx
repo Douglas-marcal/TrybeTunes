1731198df1ef199c7e346aaf901b6c24949bffd8
Douglas Marcal <douglasymarcal@gmail.com>
2022-02-05 11:02:13 -0300

    Faz requisicao para trazer as musicas do album selecionado

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 import { Header } from '../components';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  constructor() {
+    super();
+
+    this.requestMusic = this.requestMusic.bind(this);
+  }
+
+  componentDidMount() {
+    this.requestMusic();
+  }
+
+  requestMusic() {
+    const { match: { params: { id } } } = this.props;
+    getMusics(id).then((data) => data);
+  }
+
   render() {
     return (
       <div data-testid="page-album">
