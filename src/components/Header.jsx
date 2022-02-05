44419038eb4c398428ad5d8294bba9c74375eddc
Douglas Marcal <douglasymarcal@gmail.com>
2022-02-05 13:21:43 -0300

    Estiliza o `Header`

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
+import '../styles/Header.css';
 
 class Header extends Component {
   constructor() {
@@ -29,14 +30,23 @@ class Header extends Component {
   render() {
     const { name, isLoading } = this.state;
     return (
-      <header data-testid="header-component">
-        {
-          isLoading
-            ? <Loading />
-            : (<h4 data-testid="header-user-name">{name}</h4>)
-        }
-        <nav>
-          <ul>
+      <header data-testid="header-component" className="header-container">
+        <h2>Só Sucesso ⚡</h2>
+        <div className="nav-bar">
+          {
+            isLoading
+              ? <Loading />
+              : (
+                <h4
+                  data-testid="header-user-name"
+                  className="user-name"
+                >
+                  <span className="separator">|</span>
+                  {name}
+                </h4>
+              )
+          }
+          <ul className="menu-container">
             <Link to="/search">
               <li data-testid="link-to-search">Pesquisa</li>
             </Link>
@@ -47,7 +57,7 @@ class Header extends Component {
               <li data-testid="link-to-profile">Perfil</li>
             </Link>
           </ul>
-        </nav>
+        </div>
       </header>
     );
   }
