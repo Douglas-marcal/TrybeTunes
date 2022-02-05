f0c876e0aae0517a0f9c4e7f1108e9fbea9aea54
Douglas Marcal <douglasymarcal@gmail.com>
2022-02-04 23:25:00 -0300

    Chama a funcao `getUser` ao renderizar o componente

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
 
 class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+    };
+
+    this.getUserName = this.getUserName.bind(this);
+  }
+
+  componentDidMount() {
+    this.getUserName();
+  }
+
+  getUserName() {
+    getUser().then(({ name }) => {
+      this.setState(() => ({ name }));
+    });
+  }
+
   render() {
+    const { name } = this.state;
     return (
-      <header data-testid="header-component" />
+      <header data-testid="header-component">
+        <h4>{name}</h4>
+      </header>
     );
   }
 }
