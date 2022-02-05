9130eda4442adb87e6eb0f5ae99c0b2f8436b698
Douglas Marcal <douglasymarcal@gmail.com>
2022-02-05 00:58:37 -0300

    Conclui requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,15 +2,41 @@ import React, { Component } from 'react';
 import { Header } from '../components';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      buttonDisabled: true,
+      textFieldLetters: '',
+    };
+
+    this.validateInput = this.validateInput.bind(this);
+  }
+
+  validateInput({ target: { value } }) {
+    this.setState(() => ({
+      textFieldLetters: value,
+    }), () => this.setState((prevState) => ({
+      buttonDisabled: prevState.textFieldLetters.length < 2,
+    })));
+  }
+
   render() {
+    const { buttonDisabled, textFieldLetters } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <form>
-          <input data-testid="search-artist-input" type="text" />
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            value={ textFieldLetters }
+            onChange={ this.validateInput }
+          />
           <button
             data-testid="search-artist-button"
             type="button"
+            disabled={ buttonDisabled }
           >
             Pesquisar
           </button>
