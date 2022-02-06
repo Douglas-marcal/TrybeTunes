f404964819cfb01a56b1123f44cf3dba9cf08b4f
Douglas Marcal <douglasymarcal@gmail.com>
2022-02-06 16:28:28 -0300

    Adiciona informacoes usuario na tela

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,58 @@
 import React, { Component } from 'react';
-import { Header } from '../components';
+import { Header, Loading } from '../components';
 import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: true,
+      userInfo: {},
+    };
+  }
+
   componentDidMount() {
-    getUser().then((data) => data);
+    getUser().then((userInfo) => {
+      this.setState({ userInfo, isLoading: false });
+    });
   }
 
   render() {
+    const { userInfo, isLoading } = this.state;
     return (
-      <div data-testid="page-profile">
+      <div>
         <Header />
+
+        {
+          isLoading ? <Loading /> : (
+            <div data-testid="page-profile">
+
+              <img
+                data-testid="profile-image"
+                src={ userInfo.image }
+                alt={ `Foto de ${userInfo.name}` }
+              />
+
+              <p>
+                <span>Nome: </span>
+                {userInfo.name}
+              </p>
+              <p>
+                <span>E-mail: </span>
+                {userInfo.email}
+              </p>
+              <p>
+                <span>Descrição: </span>
+                {userInfo.description}
+              </p>
+            </div>
+
+          )
+        }
+
       </div>
+
     );
   }
 }
