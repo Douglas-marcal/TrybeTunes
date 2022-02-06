d5ced138876f2a5fa42764b7e895e449f924629d
Douglas Marcal <douglasymarcal@gmail.com>
2022-02-06 16:50:53 -0300

    Cria input para alteracao do nome

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
-import { Header } from '../components';
+import { Header, Loading } from '../components';
+import { getUser } from '../services/userAPI';
 
 class ProfileEdit extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+      userInfo: {},
+    };
+
+    this.updateInfos = this.updateInfos.bind(this);
+  }
+
+  componentDidMount() {
+    getUser().then((userInfo) => {
+      const { name, email, image, description } = userInfo;
+      this.setState({ name, email, image, description, isLoading: false });
+    });
+  }
+
+  updateInfos({ target: { value, name } }) {
+    this.setState(() => ({ [name]: value }));
+  }
+
   render() {
+    const { isLoading, name } = this.state;
     return (
       <div data-testid="page-profile-edit">
         <Header />
+        {
+          isLoading ? <Loading /> : (
+            <form>
+              <input
+                data-testid="edit-input-name"
+                type="text"
+                name="name"
+                value={ name }
+                onChange={ this.updateInfos }
+                placeholder="Nome"
+              />
+
+            </form>
+          )
+        }
       </div>
     );
   }
