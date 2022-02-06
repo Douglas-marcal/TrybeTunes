c878b7371154538394f4ea04ec34cb37ba0c94b7
Douglas Marcal <douglasymarcal@gmail.com>
2022-02-06 18:19:16 -0300

    Conclui requisito 14

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import { Header, Loading } from '../components';
-import { getUser } from '../services/userAPI';
+import { getUser, updateUser } from '../services/userAPI';
 
 class ProfileEdit extends Component {
   constructor() {
@@ -12,12 +13,15 @@ class ProfileEdit extends Component {
 
     this.updateInfos = this.updateInfos.bind(this);
     this.checkButton = this.checkButton.bind(this);
+    this.saveChanges = this.saveChanges.bind(this);
   }
 
   componentDidMount() {
     getUser().then((userInfo) => {
       const { name, email, image, description } = userInfo;
-      this.setState({ name, email, image, description, isLoading: false });
+      this.setState({ name, email, image, description, isLoading: false }, () => {
+        this.checkButton();
+      });
     });
   }
 
@@ -30,7 +34,16 @@ class ProfileEdit extends Component {
       && description.length > 0
     ) {
       this.setState({ isDisabled: false });
-    }
+    } else this.setState({ isDisabled: true });
+  }
+
+  saveChanges() {
+    const { name, email, image, description } = this.state;
+    this.setState({ isLoading: true }, () => {
+      updateUser({ name, email, image, description }).then(() => {
+        this.setState({ redirect: true });
+      });
+    });
   }
 
   updateInfos({ target: { value, name } }) {
@@ -38,61 +51,77 @@ class ProfileEdit extends Component {
   }
 
   render() {
-    const { isLoading, name, email, description, image, isDisabled } = this.state;
+    const {
+      isLoading,
+      name,
+      email,
+      description,
+      image,
+      isDisabled,
+      redirect,
+    } = this.state;
+
+    if (redirect) return (<Redirect to="/profile" />);
+
     return (
-      <div data-testid="page-profile-edit">
+      <div>
         <Header />
+
         {
           isLoading ? <Loading /> : (
-            <form>
-              <input
-                data-testid="edit-input-name"
-                type="text"
-                name="name"
-                value={ name }
-                onChange={ this.updateInfos }
-                placeholder="Nome"
-              />
+            <div data-testid="page-profile-edit">
+              <form>
+                <input
+                  data-testid="edit-input-name"
+                  type="text"
+                  name="name"
+                  value={ name }
+                  onChange={ this.updateInfos }
+                  placeholder="Nome"
+                />
 
-              <input
-                data-testid="edit-input-email"
-                type="text"
-                name="email"
-                value={ email }
-                onChange={ this.updateInfos }
-                placeholder="E-mail"
-              />
+                <input
+                  data-testid="edit-input-email"
+                  type="text"
+                  name="email"
+                  value={ email }
+                  onChange={ this.updateInfos }
+                  placeholder="E-mail"
+                />
 
-              <input
-                data-testid="edit-input-description"
-                type="text"
-                name="description"
-                value={ description }
-                onChange={ this.updateInfos }
-                placeholder="Descrição"
-              />
+                <input
+                  data-testid="edit-input-description"
+                  type="text"
+                  name="description"
+                  value={ description }
+                  onChange={ this.updateInfos }
+                  placeholder="Descrição"
+                />
 
-              <input
-                data-testid="edit-input-image"
-                type="text"
-                name="image"
-                value={ image }
-                onChange={ this.updateInfos }
-                placeholder="Imagem"
-              />
+                <input
+                  data-testid="edit-input-image"
+                  type="text"
+                  name="image"
+                  value={ image }
+                  onChange={ this.updateInfos }
+                  placeholder="Imagem"
+                />
 
-              <button
-                data-testid="edit-button-save"
-                type="button"
-                onClick={ this.saveChanges }
-                disabled={ isDisabled }
-              >
-                Salvar alterações
-              </button>
+                <button
+                  data-testid="edit-button-save"
+                  type="button"
+                  onClick={ this.saveChanges }
+                  disabled={ isDisabled }
+                >
+                  Editar perfil
+                </button>
 
-            </form>
+              </form>
+
+            </div>
           )
         }
+
       </div>
     );
   }
