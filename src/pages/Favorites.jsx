8bf99382544d18867d32b2369cb2d9aa659871a8
Douglas Marcal <douglasymarcal@gmail.com>
2022-02-06 14:01:43 -0300

    Renderiza lista de musicas favoritas

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
-import { Header } from '../components';
+import { Header, Loading, MusicCard } from '../components';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Favorites extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: true,
+      favoriteSongs: [],
+    };
+  }
+
+  componentDidMount() {
+    getFavoriteSongs().then((favoriteSongs) => {
+      this.setState(() => ({
+        favoriteSongs,
+        isLoading: false,
+      }));
+    });
+  }
+
   render() {
+    const { isLoading, favoriteSongs } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
+        {
+          isLoading ? <Loading /> : <MusicCard playlist={favoriteSongs}/>
+        }
       </div>
     );
   }
