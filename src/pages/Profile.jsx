8196994a8a6ea28ce2d07883d207702d93f698be
Douglas Marcal <douglasymarcal@gmail.com>
2022-02-06 15:44:49 -0300

    Recupera informacoes do usuario

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { Header } from '../components';
+import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
+  componentDidMount() {
+    getUser().then((data) => data);
+  }
+
   render() {
     return (
       <div data-testid="page-profile">
